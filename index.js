684783484f1682cf50b27844c5a84c468aae6604
Jose Martinez <jmartinezg.ca@gmail.com>
2020-08-30 19:20:34 -0700

    Initial commit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const cors = require('cors');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const postRouter = require('./routers/post');
-const commentRouter = require('./routers/comment');
-const frRouter = require('./routers/fr');
+require('./src/db/mongoose');
+const userRouter = require('./src/routers/user');
+const postRouter = require('./src/routers/post');
+const commentRouter = require('./src/routers/comment');
+const frRouter = require('./src/routers/fr');
 
 const app = express();
 const port = process.env.PORT;
@@ -19,13 +19,13 @@ app.use(postRouter);
 app.use(commentRouter);
 app.use(frRouter);
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-}
+// }
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
