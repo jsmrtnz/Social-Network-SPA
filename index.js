f7c5b3519e608fedcd95cba424d4de39e9d07b5e
Jose Martinez <jmartinezg.ca@gmail.com>
2020-08-31 21:50:33 -0700

    Added env setting

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ app.use(postRouter);
 app.use(commentRouter);
 app.use(frRouter);
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-// }
+}
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
